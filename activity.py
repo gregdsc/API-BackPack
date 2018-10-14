fbda538fb695a9ad36be76e8ccb97f0e4f9499aa
alexandre pape <alexandre.pape@epitech.eu>
2018-10-14 16:04:23 +0200

    Calorie done Speed done Km done Pas done

--- activity.py
+++ activity.py
@@ -13,39 +13,75 @@ activity_field = {
     'name': fields.String,
     'start_time': fields.String,
     'end_time': fields.String,
-    'count': fields.Integer,
-    'distance': fields.Float,
+    'pas': fields.Integer,
+    'km': fields.Float,
     'calorie': fields.Float,
     'speed': fields.Float,
     'type': fields.String,
     'uri': fields.Url('activity', absolute=True),
 }
 
-calorie_field = {
+calorie_fields = {
     'id': fields.Integer,
     'calorie': fields.Float,
     'start_time': fields.DateTime,
     'uri': fields.Url('calorie', absolute=True),
 }
 
-speed_field = {
+calorie_field = {
+    'calorie': fields.Float,
+    'start_time': fields.DateTime
+}
+
+speed_fields = {
     'id': fields.Integer,
     'speed': fields.Float,
     'start_time': fields.DateTime,
     'uri': fields.Url('speed', absolute=True),
 }
 
+speed_field = {
+    'speed': fields.Float,
+    'start_time': fields.DateTime
+}
+
+pas_fields = {
+    'id': fields.Integer,
+    'pas': fields.Float,
+    'start_time': fields.DateTime,
+    'uri': fields.Url('pas', absolute=True),
+}
+
+pas_field = {
+    'pas': fields.Float,
+    'start_time': fields.DateTime
+}
+
+km_fields = {
+    'id': fields.Integer,
+    'km': fields.Float,
+    'start_time': fields.DateTime,
+    'uri': fields.Url('km', absolute=True),
+}
+
+km_field = {
+    'km': fields.Float,
+    'start_time': fields.DateTime
+}
+
 authBasic = HTTPBasicAuth()
 authToken = HTTPTokenAuth()
 
+
+# ACTIVITIES #
 
 class ActivityResource(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('name', type=str)
     parser.add_argument('start_time', type=str)
     parser.add_argument('end_time', type=str)
-    parser.add_argument('count', type=int)
-    parser.add_argument('distance', type=float)
+    parser.add_argument('pas', type=int)
+    parser.add_argument('km', type=float)
     parser.add_argument('calorie', type=float)
     parser.add_argument('speed', type=float)
     parser.add_argument('type', type=str)
@@ -65,27 +101,30 @@ class ActivityResource(Resource):
         name = parsed_args['name']
         start_time = parsed_args['start_time']
         end_time = parsed_args['end_time']
-        count = parsed_args['count']
-        distance = parsed_args['distance']
+        pas = parsed_args['pas']
+        km = parsed_args['km']
         calorie = parsed_args['calorie']
         speed = parsed_args['speed']
         type = parsed_args['type']
 
-        if name is None or start_time is None or end_time is None or count is None or distance is None or calorie \
+        if name is None or start_time is None or end_time is None or pas is None or km is None or calorie \
                 is None or speed is None or type is None:
             abort(400, message="Missing arguments")
         activity = Activity(name=name, start_time=start_time, end_time=end_time,
-                            count=count, distance=distance, calorie=calorie, speed=speed, type=type,
+                            pas=pas, km=km, calorie=calorie, speed=speed, type=type,
                             username=g.user.username)
         session.add(activity)
         session.commit()
         return activity, 200, {'Access-Control-Allow-Origin': '*'}
 
 
+# ACTIVITY CALORIE #
+
+
 class ActivityCalorie(Resource):
 
     @authToken.login_required
-    @marshal_with(calorie_field)
+    @marshal_with(calorie_fields)
     def get(self):
         calorie_date = session.query(Activity).all()
         if not calorie_date:
@@ -93,30 +132,67 @@ class ActivityCalorie(Resource):
         return calorie_date
 
     @authToken.login_required
-    @marshal_with(calorie_field)
+    @marshal_with(calorie_fields)
     def post(self):
         parsed_args = self.parser.parse_args()
         calorie = parsed_args['calorie']
         start_time = parsed_args['start_time']
         if start_time is None or calorie is None:
             abort(400, message="Missing arguments")
-        activity = Activity(calorie=calorie,start_time=start_time, username=g.user.username)
+        activity = Activity(calorie=calorie, start_time=start_time, username=g.user.username)
         session.add(activity)
         session.commit()
         return activity, 200, {'Access-Control-Allow-Origin': '*'}
 
+
+class ActivityCalorieMax(Resource):
+
+    @authToken.login_required
+    @marshal_with(calorie_field)
+    def get(self):
+        Calorie = session.query(Activity).order_by(Activity.calorie.desc()).first()
+        if not Calorie:
+            abort(404, message="Please enter an activity with calorie before")
+        return Calorie, 200, {'Access-Control-Allow-Origin': '*'}
+
+
+class ActivityCalorieDesc(Resource):
+
+    @authToken.login_required
+    @marshal_with(calorie_field)
+    def get(self):
+        Calorie = session.query(Activity).order_by(Activity.calorie.desc()).all()
+        if not Calorie:
+            abort(404, message="Please enter an activity with calorie before")
+        return Calorie, 200, {'Access-Control-Allow-Origin': '*'}
+
+
+class ActivityCalorieMin(Resource):
+
+    @authToken.login_required
+    @marshal_with(calorie_field)
+    def get(self):
+        Calorie = session.query(Activity).order_by(Activity.calorie.asc()).first()
+        if not Calorie:
+            abort(404, message="Please enter an activity with calorie before")
+        return Calorie, 200, {'Access-Control-Allow-Origin': '*'}
+
+
+# ACTIVITY SPEEDS #
+
+
 class ActivitySpeed(Resource):
 
     @authToken.login_required
-    @marshal_with(speed_field)
+    @marshal_with(speed_fields)
     def get(self):
-        speed_date = session.query(Activity).all()
+        speed_date = session.query(Activity).order_by(Activity.id.desc()).all()
         if not speed_date:
-            abort(404, message="Please enter an activity with calorie before")
-        return speed_date
+            abort(404, message="Please enter an activity with speed before")
+        return speed_date, 200, {'Access-Control-Allow-Origin': '*'}
 
     @authToken.login_required
-    @marshal_with(speed_field)
+    @marshal_with(speed_fields)
     def post(self):
         parsed_args = self.parser.parse_args()
         speed = parsed_args['speed']
@@ -127,6 +203,155 @@ class ActivitySpeed(Resource):
         session.add(activity)
         session.commit()
         return activity, 200, {'Access-Control-Allow-Origin': '*'}
+
+
+class ActivitySpeedMax(Resource):
+
+    @authToken.login_required
+    @marshal_with(speed_field)
+    def get(self):
+        speeds = session.query(Activity).order_by(Activity.speed.desc()).first()
+        if not speeds:
+            abort(404, message="Please enter an activity with speed before")
+        return speeds, 200, {'Access-Control-Allow-Origin': '*'}
+
+
+class ActivitySpeedDesc(Resource):
+
+    @authToken.login_required
+    @marshal_with(speed_field)
+    def get(self):
+        speeds = session.query(Activity).order_by(Activity.speed.desc()).all()
+        if not speeds:
+            abort(404, message="Please enter an activity with speed before")
+        return speeds, 200, {'Access-Control-Allow-Origin': '*'}
+
+
+class ActivitySpeedMin(Resource):
+
+    @authToken.login_required
+    @marshal_with(speed_field)
+    def get(self):
+        speeds = session.query(Activity).order_by(Activity.speed.asc()).first()
+        if not speeds:
+            abort(404, message="Please enter an activity with speed before")
+        return speeds, 200, {'Access-Control-Allow-Origin': '*'}
+
+
+# ACTIVITY PAS #
+
+class ActivityPas(Resource):
+
+    @authToken.login_required
+    @marshal_with(pas_fields)
+    def get(self):
+        pas = session.query(Activity).order_by(Activity.pas.desc()).all()
+        if not pas:
+            abort(404, message="Please enter an activity with Number Pas before")
+        return pas, 200, {'Access-Control-Allow-Origin': '*'}
+
+    @authToken.login_required
+    @marshal_with(pas_fields)
+    def post(self):
+        parsed_args = self.parser.parse_args()
+        pas = parsed_args['pas']
+        start_time = parsed_args['start_time']
+        if start_time is None or pas is None:
+            abort(400, message="Missing arguments")
+        activity = Activity(pas=pas, start_time=start_time, username=g.user.username)
+        session.add(activity)
+        session.commit()
+        return activity, 200, {'Access-Control-Allow-Origin': '*'}
+
+
+class ActivityPasMax(Resource):
+
+    @authToken.login_required
+    @marshal_with(pas_field)
+    def get(self):
+        pas = session.query(Activity).order_by(Activity.pas.desc()).first()
+        if not pas:
+            abort(404, message="Please enter an activity with Number Pas before")
+        return pas, 200, {'Access-Control-Allow-Origin': '*'}
+
+
+class ActivityPasDesc(Resource):
+
+    @authToken.login_required
+    @marshal_with(speed_field)
+    def get(self):
+        pas = session.query(Activity).order_by(Activity.pas.desc()).all()
+        if not pas:
+            abort(404, message="Please enter an activity with Number Pas before")
+        return pas, 200, {'Access-Control-Allow-Origin': '*'}
+
+
+class ActivityPasMin(Resource):
+
+    @authToken.login_required
+    @marshal_with(speed_field)
+    def get(self):
+        pas = session.query(Activity).order_by(Activity.pas.asc()).first()
+        if not pas:
+            abort(404, message="Please enter an activity with Number Pas before")
+        return pas, 200, {'Access-Control-Allow-Origin': '*'}
+
+    # ACTIVITY KM #
+
+
+class ActivityKm(Resource):
+
+    @authToken.login_required
+    @marshal_with(km_fields)
+    def get(self):
+        km = session.query(Activity).order_by(Activity.km.desc()).all()
+        if not km:
+            abort(404, message="Please enter an activity with km before")
+        return km, 200, {'Access-Control-Allow-Origin': '*'}
+
+    @authToken.login_required
+    @marshal_with(km_fields)
+    def post(self):
+        parsed_args = self.parser.parse_args()
+        km = parsed_args['km']
+        start_time = parsed_args['start_time']
+        if start_time is None or km is None:
+            abort(400, message="Missing arguments")
+        activity = Activity(km=km, start_time=start_time, username=g.user.username)
+        session.add(activity)
+        session.commit()
+        return activity, 200, {'Access-Control-Allow-Origin': '*'}
+
+
+class ActivityKmMax(Resource):
+
+    @authToken.login_required
+    @marshal_with(km_field)
+    def get(self):
+        km = session.query(Activity).order_by(Activity.km.desc()).first()
+        if not km:
+            abort(404, message="Please enter an activity with km before")
+        return km, 200, {'Access-Control-Allow-Origin': '*'}
+
+class ActivityKmDesc(Resource):
+
+    @authToken.login_required
+    @marshal_with(km_field)
+    def get(self):
+        km = session.query(Activity).order_by(Activity.km.desc()).all()
+        if not km:
+            abort(404, message="Please enter an activity with km before")
+        return km, 200, {'Access-Control-Allow-Origin': '*'}
+
+class ActivityKmMin(Resource):
+
+    @authToken.login_required
+    @marshal_with(km_field)
+    def get(self):
+        km = session.query(Activity).order_by(Activity.km.asc()).first()
+        if not km:
+            abort(404, message="Please enter an activity with km before")
+        return km, 200, {'Access-Control-Allow-Origin': '*'}
 
 
 @authToken.verify_token
--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask_restful import Api
 from resources import UserListRessource, UserResource, InterestPointListRessource, InterestPointRessource, GetToken
-from activity import ActivityResource, ActivityCalorie, ActivitySpeed
+from activity import *
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -25,8 +25,31 @@ api.add_resource(GetToken, '/token', endpoint='token')
 
 # Activity #
 api.add_resource(ActivityResource, '/', '/activity', '/activity/', endpoint='activity')
+
+# Activity Calories #
 api.add_resource(ActivityCalorie, '/activity/calorie/', endpoint='calorie')
+api.add_resource(ActivityCalorieMax, '/activity/calorie_max', endpoint='calorie_max')
+api.add_resource(ActivityCalorieDesc, '/activity/calorie_desc', endpoint='calorie_desc')
+api.add_resource(ActivityCalorieMin, '/activity/calorie_min', endpoint='calorie_min')
+
+# Activity Speeds #
 api.add_resource(ActivitySpeed, '/activity/speed/', endpoint='speed')
+api.add_resource(ActivitySpeedMax, '/activity/speed_max', endpoint='speed_max')
+api.add_resource(ActivitySpeedDesc, '/activity/speed_desc', endpoint='speed_desc')
+api.add_resource(ActivitySpeedMin, '/activity/speed_min', endpoint='speed_min')
+
+# Activity Pas #
+api.add_resource(ActivityPas, '/activity/pas/', endpoint="pas")
+api.add_resource(ActivityPasMax, '/activity/pas_max', endpoint='pas_max')
+api.add_resource(ActivityPasDesc, '/activity/pas_desc', endpoint='pas_desc')
+api.add_resource(ActivityPasMin, '/activity/pas_min', endpoint='pas_min')
+
+# Activity Km #
+api.add_resource(ActivityKm, '/activity/km/', endpoint='km')
+api.add_resource(ActivityKmMax, '/activity/km_max', endpoint='km_max')
+api.add_resource(ActivityKmDesc, '/activity/km_desc', endpoint='km_desc')
+api.add_resource(ActivityKmMin, '/activity/km_min', endpoint='km_min')
+
 
 
 if __name__ == '__main__':
