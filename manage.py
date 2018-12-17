7dc14501533febcbba9d23b55900036c3c9be050
Alex <alexandre.pape@epitech.eu>
2018-12-18 00:14:01 +0100

    Problème merge correctif

--- manage.py
+++ manage.py
@@ -27,6 +27,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), index=True)
     password_hash = db.Column(db.String(255))
+    description = db.Column(db.String(500))
+    pic_url = db.Column(db.String(255))
 
     def hash_password(self, password):
         self.password_hash = pwd_context.encrypt(password)
@@ -67,6 +69,7 @@ class InterestPoint(db.Model):
     long = db.Column(db.Float)
     userName = db.Column(db.String(255))
     type = db.Column(db.String(255))
+    rank = db.Column(db.Integer)
     imageUrls = []
 
 
@@ -97,8 +100,6 @@ class Ramble(db.Model):
     travel_time = db.Column(db.Float)
     step_number = db.Column(db.Float)
     ramble_d = db.relationship('Ramble_details', backref='ramble_details', lazy='dynamic')
-
-
 
 
 class Ramble_details(db.Model):
--- resources.py
+++ resources.py
@@ -16,6 +16,8 @@ user_fields = {
     'id': fields.Integer,
     'username': fields.String,
     'password_hash': fields.String,
+    'description': fields.String,
+    'pic_url': fields.String,
     'uri': fields.Url('user', absolute=True),
 
 }
@@ -28,6 +30,7 @@ interest_field = {
     'long': fields.Float,
     'userName': fields.String,
     'type': fields.String,
+    'rank': fields.Integer,
     'imageUrls': fields.List(fields.String(attribute='url')),
     'uri': fields.Url('poi', absolute=True),
 }
@@ -40,6 +43,8 @@ class UserResource(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('username', type=str)
     parser.add_argument('password_change', type=dict)
+    parser.add_argument('new_description', type=str)
+    parser.add_argument('new_pic_url', type=str)
 
     @marshal_with(user_fields)
     def get(self, id):
@@ -63,21 +68,19 @@ class UserResource(Resource):
     @marshal_with(user_fields)
     def put(self, id):
         parsed_args = self.parser.parse_args()
-        print(parsed_args)
         user = session.query(User).filter(User.id == id).first()
         if user.id != g.user.id:
             abort(403, message="user {0} is not allowed to modify user {1}".format(id, g.user.id))
         if parsed_args['username'] is not None:
             user.username = parsed_args['username']
-            print("lel")
+        if parsed_args['new_description'] is not None:
+            user.description = parsed_args['new_description']
         if parsed_args['password_change'] is not None:
             password_reset = parsed_args['password_change']
             if not user.verify_password(password_reset['old_password']) or password_reset['new_password'] != \
                     password_reset['new_password_confirm']:
                 abort(400, message="wrong old_password or new_password and new_password_confirm mismatch")
             user.hash_password(password_reset['new_password'])
-        else:
-            abort(403, message="password has not change")
         session.add(user)
         session.commit()
         return user, 201
@@ -86,6 +89,8 @@ class UserListRessource(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('username', type=str)
     parser.add_argument('password', type=str)
+    parser.add_argument('description', type=str)
+    parser.add_argument('pic_url', type=str)
 
     @marshal_with(user_fields)
     def get(self):
@@ -99,12 +104,20 @@ class UserListRessource(Resource):
         parsed_args = self.parser.parse_args()
         username = parsed_args['username']
         password = parsed_args['password']
+        description = parsed_args['description']
+
         if username is None or password is None:
             abort(400, message="Missing arguments")
         if session.query(User).filter(User.username == username).first() is not None:
             abort(400, message="User {} already exists".format(username))
         user = User(username=username)
         user.hash_password(password)
+        if description is not None:
+           user.description = description
+        image = request.files.getlist('image')
+        if image is not None:
+            cloudinary_struct = uploader.upload(image[0], public_id='{0}_{1}'.format(username, image[0].filename))
+            user.pic_url = cloudinary_struct['url']
         session.add(user)
         session.commit()
         return user, 201
@@ -117,6 +130,7 @@ class InterestPointfiltre(Resource):
     parser.add_argument('lat', type=float)
     parser.add_argument('long', type=float)
     parser.add_argument('type', type=str)
+    parser.add_argument('rank', type=int)
 
 
     @marshal_with(interest_field)
@@ -165,6 +179,12 @@ class InterestPointRessource(Resource):
         poi.description = parsed_args['description']
         poi.lat = parsed_args['lat']
         poi.long = parsed_args['long']
+        rank = parsed_args['rank']
+        if rank is not None:
+            if rank < 1 or rank > 5:
+                abort(400, message="rank should be between 1 to 5")
+            else:
+                poi.rank = rank
         session.add(poi)
         session.commit()
         return poi, 201
@@ -177,6 +197,7 @@ class InterestPointListRessource(Resource):
     parser.add_argument('lat', type=float)
     parser.add_argument('long', type=float)
     parser.add_argument('type', type=str)
+    parser.add_argument('rank', type=int)
 
     @marshal_with(interest_field)
     def get(self):
@@ -195,18 +216,25 @@ class InterestPointListRessource(Resource):
         lat = parsed_args['lat']
         long = parsed_args['long']
         type = parsed_args['type']
+        rank = parsed_args['rank']
+
         if name is None or description is None or lat is None or long is None:
             abort(400, message="Missing arguments")
         if session.query(InterestPoint).filter(InterestPoint.name == name).first() is not None:
             abort(400, message="Poi {} already exists".format(name))
         poi = InterestPoint(name=name, description=description, lat=lat,
                             long=long, type=type, userName=g.user.username)
+        if rank is not None:
+            if rank < 1 or rank > 5:
+                abort(400, message="rank should be between 1 to 5")
+            else:
+                poi.rank = rank
         session.add(poi)
         poi.imageUrls = []
         images = request.files.getlist('images')
         for image in images:
             cloudinary_struct = uploader.upload(image, public_id='{0}_{1}_{2}'.format(g.user.username, name,
-                                                                                      image.filename))
+                                                                                        image.filename))
             print(cloudinary_struct)
             url = ImageUrls(url=cloudinary_struct['url'], poiName=name)
             poi.imageUrls.append(url)
@@ -220,7 +248,9 @@ class GetToken(Resource):
     @authBasic.login_required
     def get(self):
         token = g.user.generate_auth_token(expiration=10000)
-        return {'token': token.decode('ascii')}
+        return {'id_user': g.user.id,
+                'token': token.decode('ascii')
+                }
 
 
 @authBasic.verify_password
