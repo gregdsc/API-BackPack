569f2ca67888b1809f9720baa083649aab2e4d61
alexandre <a.pape@opentlv.com>
2019-05-21 12:51:03 +0200

    Architecture clean

---
+++ API/Source/Activity/View/Model/model_activity.py
@@ -0,0 +1,16 @@
+from Source.User.Model.model_user import *
+
+class Activity(db.Model):
+
+    _tablename__ = 'activity'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.String(255))
+    pas = db.Column(db.Integer)
+    km = db.Column(db.Float)
+    calorie = db.Column(db.Float)
+    speed = db.Column(db.Float)
+    type = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
