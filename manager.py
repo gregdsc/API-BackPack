0353bcf2539dcf5de485a90eb3243ab410caed74
alexandre <a.pape@opentlv.com>
2019-05-21 14:53:46 +0200

    Architecture clean

--- manager.py
+++ manager.py
@@ -9,7 +9,6 @@ app = create_app(os.getenv('FLASK_CONFIG'))
 with app.app_context():
     from Source.Ramble.Model.model_ramble import *
     from Source.User.Model.model_user import *
-    from Source.Point.Model.model_point import *
     from Source.Activity.Model.model_activity import *
     from Source.Comment.Model.model_comment import *
 
