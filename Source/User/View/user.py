3e879dbbc8a189fdd7845e5983e5a3747262e89e
alexandre <a.pape@opentlv.com>
2019-07-07 18:32:34 +0200

    test google vision sur heroku

--- Source/User/View/user.py
+++ Source/User/View/user.py
@@ -33,7 +33,7 @@ from google.oauth2 import service_account
 
 
 #credentials = service_account.Credentials. from_service_account_file(r'C:\Users\Alexandre\PycharmProjects\Backpack-api\project.json')
-credentials = service_account.Credentials. from_service_account_file(r'app\project.json')
+credentials = service_account.Credentials. from_service_account_file(r'project.json')
 
 #clients = vision.ImageAnnotatorClient(credentials=credentials)
 
