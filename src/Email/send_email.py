2c6e690139078c4327f358501f403cdf7f3de46a
Grégoire <gregoire.descombris@epitech.eu>
2019-09-12 19:47:20 +0800

    fix mail

--- src/Email/send_email.py
+++ src/Email/send_email.py
@@ -3,19 +3,19 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
-
 def send_mail(send_from, subject, send_to, html):
     send_to = send_to
-
     msg = MIMEMultipart()
     msg['From'] = send_from
-    msg['To'] = ','.join(send_to)
+    msg['To'] = ', '.join(send_to)
     msg['Subject'] = subject
-
     msg.attach(MIMEText(html, 'html'))
-
-    protocol_mail_transfer = smtplib.SMTP(host='smtp.gmail.com', port=587)
-    protocol_mail_transfer.starttls()
-    protocol_mail_transfer.login('noreply.backpack@gmail.com', 'Wpn%:)z5"KP4-&6Av,XodGC-dW8V')  # Env
-    protocol_mail_transfer.sendmail(send_from, send_to, msg.as_string())
-    protocol_mail_transfer.close()
+    smtp = smtplib.SMTP(host='SSL0.OVH.NET', port=587)
+    smtp.starttls()
+    smtp.login('contact@pick-me.eu', 'testapipython')
+    try:
+        smtp.sendmail(send_from, send_to, msg.as_string())
+    except:
+        return 'Mail non accepté'
+    finally:
+        smtp.close()
